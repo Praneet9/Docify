a87932901df210b202b5cbd11b9d6f7924cbda2a
Praneet9 <praneetbomma@gmail.com>
2019-01-24 08:48:43 +0530

    commented code description :fire:

--- api/processing.py
+++ api/processing.py
@@ -7,40 +7,60 @@ from keras.models import model_from_json
 import tensorflow as tf
 from keras import backend as k
 
+
+# function to recognise text from image
 def recognise_text(image_path, photo_path):
     
+    # read image and convert to grayscale
     image = cv2.imread(image_path, 0)
 
+    # get coordinates of text using ctpn
     coordinates = get_coords(image_path)
 
     detected_text = []
 
+    # sorting coordinates from top to bottom
     coordinates = sorted(coordinates, key = lambda coords: coords[1])
 
+    # looping through all the text boxes
     for coords in coordinates:
+        # x, y, width, height of the text box
         x, y, w, h = coords
+
+        # cropping image based on the coordinates
         temp = image[y:h, x:w]
 
+        # binarizing image
         _, thresh = cv2.threshold(temp, 100, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
+        # padding the image with 10 pixels for better prediction with tesseract
         thresh = cv2.copyMakeBorder(thresh, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+        
+        # get text from the image, lang = english + hindi + marathi, config = use lstm for prediction
         text = pyt.image_to_string(thresh, lang="eng+hin+mar", config=('--oem 1 --psm 3'))
         
+        # clean text and remove noise
         text = clean_text(text)
+
+        # ignore text if the length of text is less than 3
         if len(text) < 3:
             continue
         detected_text.append(text)
 
+    # find face in the image
     face, found = get_photo(image)
 
+    # if a face is found save it to faces directory
     if found:
         cv2.imwrite(photo_path, face)
     else:
         photo_path = face
     
+    # return detected text and the face path
     return detected_text, photo_path
 
 
+# function to remove noise and unnecessary characters from string
 def clean_text(text):
     if text != ' ' or text != '  ' or text != '':
         text = re.sub('[^A-Za-z0-9-/ ]+', '', text)
@@ -50,84 +70,110 @@ def clean_text(text):
         
     return text
 
+
+# extract labels from licence image
 def get_labels_from_licence(details1):
     imp = {}
-    print(details1)
+    
+    # loop through all the details found line wise
     for idx in range(len(details1)):
+
+        # if DL No is found save it
         if 'DL No' in details1[idx]:
             try:
                 imp["DL NO"] = details1[idx].split('DL No')[-1].strip()
             except Exception as _:
                 imp["DL NO"] = "Not Found"
-            # del details1[idx]
+                
+        # if dob is found, use it as a hook and try finding other details relative to it
         elif details1[idx].startswith('DOB'):
+            # extract only dob from the text
             dob = re.findall(r"([0-9]{2}\-[0-9]{2}\-[0-9]{4})", details1[idx].split(' ', 1)[-1])[0]
-            #imp["DOB"] = details1[idx].split(' ', 1)[1].strip().split(r"[0-9]{0,2}\-[0-9]{0,2}\-[0-9]{0,4}", 1)[0]
+            
             imp["Date of Birth"] = dob
-            # del details1[idx]
+            
+            # next line is always name and father's name
             imp["Name"] = details1[idx + 1].split(' ', 1)[-1].strip()
-            # del details1[idx + 1]
+            
             try:
+                # split it from 'of' to get Father's Name
                 imp["Father's Name"] = details1[idx + 2].split('of',1)[1].strip()
-                # del details1[idx + 2]
+                
             except Exception as _:
+                # handle exception if O is capital in 'of'
                 imp["Father's Name"] = details1[idx + 2].split('Of',1)[1].strip()
-                # del details1[idx + 2]
+                
             i = 4
+            # split next line from Add for address
             address = details1[idx + 3].split('Add', 1)[1].strip()
-            # del details1[idx + 3]
+            
+            # keep appending until PIN code is found or address is of more than 4 lines
             while not details1[idx + i].startswith('PIN') and i < 8:
                 if details1[idx + i].isupper() != True:
-                    # del details1[idx + i]
+                    
                     i += 1
                     continue
                 address += ' ' + details1[idx + i]
-                # del details1[idx + i]
+                
                 i += 1
             imp["Address"] = address
             try:
+                # get only pin code from the string
                 imp["Pin Code"] = re.findall(r"([0-9]{6})", details1[idx + i].split(' ', 1)[1])[0]
             except Exception as _:
                 pass
-            # del details1[idx + i]
+                
             break
+        # if name is found, use it as a hook and try finding other details relative to it
         elif details1[idx].startswith('Name'):
+            # extract only dob from the text
             dob = re.findall(r"([0-9]{2}\-[0-9]{2}\-[0-9]{4})", details1[idx - 1].split(' ', 1)[1])[0]
             imp["Date of Birth"] = dob
-            # del details1[idx - 1]
+            
             imp["Name"] = details1[idx][4:].strip()
-            # del details1[idx]
+            
+            # next line is always name and father's name
             try:
+                # split it from 'of' to get Father's Name
                 imp["Father's Name"] = details1[idx + 2].split('of',1)[1].strip()
-                # del details1[idx + 2]
+                
             except Exception as _:
+                # handle exception if O is capital in 'of'
                 imp["Father's Name"] = details1[idx + 2].split('Of',1)[1].strip()
-                # del details1[idx + 2]
+                
             i = 3
+            # split next line from Add for address
             address = details1[idx + 2].split('Add', 1)[1].strip()
-            # del details1[idx + 2]
+            
+            # keep appending until PIN code is found or address is of more than 4 lines
             while not details1[idx + i].startswith('PIN') and i < 7:
                 if details1[idx + i].isupper() != True:
-                    # del details1[idx + i]
+                    
                     i += 1
                     continue
                 address += ' ' + details1[idx + i]
-                # del details1[idx + i]
+                
                 i += 1
             imp["Address"] = address
             try:
+                # get only pin code from the string
                 imp["Pin Code"] = re.findall(r"([0-9]{6})", details1[idx + i].split(' ', 1)[1])[0]
             except Exception as _:
                 pass
-            # del details1[idx + i]
+                
             break
     return imp
 
 
+# extract labels from aadhar image
 def get_labels_from_aadhar(temp):
     imp = {}
+
+    # reverse list to parse through it starting from the aadhar number
     temp = temp[::-1]
+    # parse through the list
     for idx in range(len(temp)):
+        # if string similar to aadhar number is found, use it as a hook to find other details
         if re.search(r"[0-9]{4}\s[0-9]{4}\s[0-9]{4}", temp[idx]):
             try:
                 imp['Aadhar No'] = re.findall(r"[0-9]{4}\s[0-9]{4}\s[0-9]{4}", temp[idx])[0]
@@ -145,25 +191,33 @@ def get_labels_from_aadhar(temp):
                 imp["Gender"] = "Female"
             elif temp[idx + 3].endswith("Male") or temp[idx + 3].endswith("MALE"):
                 imp["Gender"] = "Male"
+
         elif re.search(r"[0-9]{2}\-|/[0-9]{2}\-|/[0-9]{4}", temp[idx]):
+            # if string similar to date is found, use it as a hook to find other details
             try:
                 imp["Date of Birth"] = re.findall(r"[0-9]{2}\-[0-9]{2}\-[0-9]{4}", temp[idx])[0]
             except Exception as _:
                 imp["Date of Birth"] = re.findall(r"[0-9]{2}/[0-9]{2}/[0-9]{4}", temp[idx])[0]
             imp["Name"] = temp[idx + 1]
+        
         elif "Year of Birth" in temp[idx]:
+            # handle variation of 'Year of Birth' in place of DOB
             try:
                 imp["Year of Birth"] = re.findall(r"[0-9]{4}", temp[idx])[0]
             except Exception as _:
                 imp["Year of Birth"] = "Not Found"
             imp["Name"] = temp[idx + 1]
+        
         elif re.search(r"[0-9]{4}", temp[idx]):
+            # handle exception if Year of Birth is not found but string similar to year is found
             try:
                 imp["Year of Birth"] = re.findall(r"[0-9]{4}", temp[idx])[0]
             except Exception as _:
                 imp["Year of Birth"] = "Not Found"
             imp["Name"] = temp[idx + 1]
+        
         elif len(temp[idx].split(' ')) > 2:
+            # following text will be name, ignore line if it includes GOVERNMENT OF INDIA
             if 'GOVERNMENT' in temp[idx] or 'OF' in temp[idx] or 'INDIA' in temp[idx]:
                 continue
             else:
@@ -172,52 +226,43 @@ def get_labels_from_aadhar(temp):
 
 
 def seven_segment(image_path):
-    
-    test = cv2.imread(image_path)
-    test = cv2.cvtColor(test, cv2.COLOR_BGR2LAB)
-    test, _, _ = cv2.split(test)
-    hist,bins = np.histogram(test,256,[0,256])
-    mean = int((np.argmax(hist) + np.argmin(hist)) / 3)
-    test[test > mean] = 255
-    test[test <= mean] = 0
-    cv2.imwrite('test.jpg', test)
-
-    test = cv2.imread(image_path)
-    test = cv2.cvtColor(test, cv2.COLOR_BGR2LAB)
-    test, _, _ = cv2.split(test)
-    test = cv2.adaptiveThreshold(test,255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,5)
-    test = cv2.fastNlMeansDenoising(test, None, 4, 7, 21)
-    cv2.imwrite('testadap.jpg', test)
-
+    # read image and convert to grayscale
     image = cv2.imread(image_path, 0)
-    cv2.imwrite('image.jpg', image)
+    
+    # calculate histogram of the image
     hist, _ = np.histogram(image,256,[0,256])
-
+    
+    # binarize image based on the peak value of histogram
     _, img = cv2.threshold(image, np.argmax(hist) - 10, 255, cv2.THRESH_BINARY_INV)
-    cv2.imwrite('thresh.jpg', img)
+    
+    # clear noise if any in the image
     noise_cleared = cv2.fastNlMeansDenoising(img, None, 4, 7, 21)
-    cv2.imwrite('denoise.jpg', noise_cleared)
+    
+    # remove border lines
     lines_removed = _lineRemoval(noise_cleared)
-    cv2.imwrite('lines.jpg', lines_removed)
+    
+    # segment character by character for prediction
     text = _character_segmentation(lines_removed)
 
     return text
 
-
+# function to find face in the image
 def get_photo(image):
-    '''
-    Image Should be 1920 x 1080 pixels
-    '''
+
+    # Image Should be 1920 x 1080 pixels
     scale_factor = 1.1
     min_neighbors = 3
     min_size = (150, 150)
     flags = cv2.CASCADE_SCALE_IMAGE
 
+    # using frontal face haar cascade
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+    # detect faces of different sizes
     faces = face_cascade.detectMultiScale(image, scaleFactor = scale_factor, minNeighbors = min_neighbors,
                                           minSize = min_size, flags = flags)
     
+    # crop the face if found
     try:
         x, y, w, h = faces[0]
         face = image[y-50:y+h+40, x-10:x+w+10]
@@ -225,9 +270,12 @@ def get_photo(image):
     except Exception as _:
         return "Photo not found!", False
 
+# function to remove border lines
 def _lineRemoval(img):
     min_length=140
+    # getting matrix of values in all the rows
     matrix = _imgToMatrixR(img)
+    # parsing through the matrix row by row
     for i in range(0, len(matrix)):
         row=matrix[i]
         start=-1
@@ -255,12 +303,11 @@ def _lineRemoval(img):
                 start = -1
                 end = 0
                 conn = 0
-#     showImage('after line', img)
+    
     return img
 
-'''
-this function convert image into matrix of image rows
-'''
+
+# this function converts image into matrix of image rows
 def _imgToMatrixR(img):
     # get dimensions
     height, width = img.shape
@@ -273,9 +320,8 @@ def _imgToMatrixR(img):
         matrix.append(row)
     return matrix
 
-'''
-this function convert image into matrix of image columns
-'''
+
+# this function convert image into matrix of image columns
 def _imgToMatrixC(img):
     # get dimensions
     height, width = img.shape
@@ -288,11 +334,10 @@ def _imgToMatrixC(img):
         matrix.append(col)
     return matrix
 
-'''
-this function clears all horizontal boundaries around the input image
-'''
+
+# this function clears all horizontal boundaries around the input image
 def clearBounds_horiz(img):
-#     showImage('before horizontal', img)
+    
     height, width = img.shape
     matrix = _imgToMatrixR(img)
     white_counter = _countPixel(matrix,255)
@@ -309,14 +354,13 @@ def clearBounds_horiz(img):
             img = img[0:new_height-i,0:width]
         else:
             break
-#     showImage('after horizontal', img)
+    
     return img
 
-'''
-this function clears all vertical boundaries around the input image
-'''
+
+# this function clears all vertical boundaries around the input image
 def clearBounds_vert(img):
-#     showImage('before vertical', img)
+    
     height, width = img.shape
     matrix = _imgToMatrixC(img)
     white_counter = _countPixel(matrix,255)
@@ -333,43 +377,50 @@ def clearBounds_vert(img):
             img = img[0:height,0:new_width-i]
         else:
             break
-#     showImage('after vertical', img)
+    
     return img
 
-'''
-this function count a specific value (parameter p) in matrix
-'''
+
+# this function counts a specific value (parameter p) in matrix
 def _countPixel(matrix,p):
     counter = []
     for k in range(0, len(matrix)):
         counter.append(matrix[k].count(p))
     return counter
 
+
+# function to segment image character by character for seven segment prediction
 def _character_segmentation(img):
     height = img.shape[0] / 3
     
+    # dilating text to fill the gaps between characters
     dilated = cv2.dilate(img, np.ones((1, 1)), iterations = 2)
+    # dilating text vertically
     dilated = cv2.dilate(img, np.ones((40, 1)), iterations = 1)
-    cv2.imwrite('dilated.jpg', dilated)
+    
     # canny = cv2.Canny(dilated, 30, 150)
     
+    # finding contours in the text
     _, ctrs_line, _ = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # sorting contours horizontally
     sorted_ctrs_line = sorted(ctrs_line, key=lambda ctr: cv2.boundingRect(ctr)[0])
     
     text = ''
 
+    # parsing through all the contours
     for ctr_line in sorted_ctrs_line:
         x_character, y_character, w_character, h_character = cv2.boundingRect(ctr_line)
         if h_character < height:
             continue
 
+        # cropping character
         cropped_line = img[y_character:y_character + h_character, x_character:x_character + w_character]
-#         cropped_line = cv2.copyMakeBorder(cropped_line, 2, 2, 2, 2, cv2.BORDER_CONSTANT)
-#         showImage('image', cropped_line)
         
+        # resizing and adding padding to final size as 32 x 32
         cropped_line = cv2.resize(cropped_line, (20, 20), None)
         cropped_line = cv2.copyMakeBorder(cropped_line, 6, 6, 6, 6, cv2.BORDER_CONSTANT)
 
+        # using predefind session for prediction
         k.set_session(session)
         with my_graph.as_default():
             output = model.predict_classes(cropped_line.reshape(-1, 32, 32, 1))
@@ -377,9 +428,12 @@ def _character_segmentation(img):
 
     return text
 
+
+# function to initialize model for prediction
 def _init_model():
     global model, my_graph, session
 
+    # creating graph with session as there are more than one deep learning models running simultaneously
     my_graph = tf.Graph()
     with my_graph.as_default():
         session = tf.Session()
--- api/server.py
+++ api/server.py
@@ -7,54 +7,78 @@ import db
 from face_matching import match_faces
 
 app = Flask(__name__)
+
+# path to upload images
 UPLOAD_FOLDER = './UPLOAD_FOLDER/'
 
+# initializing seven segment display model
 _init_model()
 
+# route to uploading images of id cards
 @app.route('/image/upload', methods=['POST'])
 def index():
     
     if request.method == 'POST':
 
+        # saving current timestamp
         current_time = str(datetime.datetime.now())
 
+        # get the type of image that is being received
         image_type = request.form['type']
         
+        # setting filename that is being received to current time stamp with its directory
         filename = UPLOAD_FOLDER + image_type + '/' + current_time + '.png'
 
+        # if the image_type folder doesn't already exist, create it
         if not os.path.exists(UPLOAD_FOLDER + image_type):
             os.mkdir(UPLOAD_FOLDER + image_type)
+            # directory for saving faces in the id cards
             os.mkdir(UPLOAD_FOLDER + image_type + '/' + 'faces')
         
+        # if image_type is bank cheque, preprocess accordingly
         if image_type == 'Bank Cheque':
             details = {}
+
+            # get photo from android
             photo = request.files['photo']
             photo.save(filename)
+
+            # get details from the image
             details['MICR'] = get_micrcode(filename)
             details['ACC.No'] = ensemble_acc_output(filename)
             details['IFSC'] = ensemble_ifsc_output(filename)
 
+            # return the details and the image name it is saved as
             return jsonify({'status':True, 'fields': details, 'image_path': filename, 'photo_path': 'none' })
 
+        # if image_type is seven segment, preprocess accordingly
         elif image_type == 'Seven Segment':
             details = {}
+
+            # get photo from android
             photo = request.files['photo']
             photo.save(filename)
 
+            # get text from seven segment
             text = seven_segment(filename)
 
             details[0] = text
 
+            # return the details and the image name it is saved as
             return jsonify({'status':True, 'fields': details, 'image_path': filename, 'photo_path': 'none' })
 
         else:
+            # setting directory for saving face in the id card
             photo_path = UPLOAD_FOLDER + image_type + '/' + 'faces' + '/' + current_time + '.png'
             
+            # get photo from android
             photo = request.files['photo']
             photo.save(filename)
 
+            # recognise text in the id card
             data, photo_path = recognise_text(filename, photo_path)
-            #details = { idx : text for idx, text in enumerate(data) }
+            
+            # extract labels from the recognised text according to the image_type
             if image_type == "Driving Licence":
                 details = { idx : text for idx, text in enumerate(data) }
                 details = get_labels_from_licence(details)
@@ -63,14 +87,17 @@ def index():
             else:
                 details = { idx : text for idx, text in enumerate(data) }
 
+            # return the details and the image name and photo path it is saved as
             return jsonify({'status':True, 'fields': details, 'image_path': filename, 'photo_path': photo_path})
     else:
+        # if not POST, terminate
         return jsonify({'status':False})
 
-
+# save data to database
 @app.route('/api/data', methods=['POST'])
 def saveData():
     
+    # get values as json
     values = request.get_json()
     image_type = values.get('type')
     data = values.get('fields')
@@ -83,21 +110,30 @@ def saveData():
 @app.route('/image/face_match',methods=['GET','POST'])
 def face_match():
 
+    # saving current timestamp
     current_time = str(datetime.datetime.now())
 
+    # temporary folder for saving face for face matching
     if not os.path.exists(UPLOAD_FOLDER + 'temp'):
             os.mkdir(UPLOAD_FOLDER + 'temp')
 
+    # setting filename that is being received to current time stamp with its directory
     filename = UPLOAD_FOLDER + 'temp' + '/' + current_time + '.png'
     
+    # getting the path of the saved face image
     photo_path = request.form['photopath']
 
+    # get live face from android
     photo = request.files['liveface']
     photo.save(filename)
     
+    # check face match and probability
     result, percent = match_faces(id_card_image=photo_path, ref_image=filename)
 
+    # delete the temp face image
     os.remove(filename)
+
+    # return face match prediction and percentage
     return jsonify({'status':str(result), 'percent': percent})
 
 
